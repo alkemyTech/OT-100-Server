c7546d11c9673e1d7da261d460d30a15151e58a1
Samuel Terrazas <samuel-terrazas@outlook.com>
2021-10-26 00:06:41 -0300

    st: Creacion tabla AuditEntityBase - BaseEntity

---
+++ OngProject.Entities/Entities/AuditEntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OngProject.Domain.Entities
+{
+    public class AuditEntityBase
+    {
+        private DateTime? _createdAt;
+        public DateTime CreatedAt
+        {
+            get => _createdAt ??= DateTime.Now;
+        }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+    }
+}
---
+++ OngProject.Entities/Entities/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace OngProject.Domain.Entities
+{
+    public abstract class BaseEntity : AuditEntityBase
+    {
+        public int Id { get; set; }
+    }
+}
