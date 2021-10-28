deafd1b0bc7e67e4d58a7c8e360563ac3f10ad0a
--replace-all <--replace-all>
2021-10-28 15:54:55 -0300

    jb: Creacion de modelos y migracion de usuarios y roles.

--- OngProject.Entities/Entities/User.cs
+++ OngProject.Entities/Entities/User.cs
@@ -9,6 +9,8 @@ namespace OngProject.Domain.Entities
         public string Email { get; set; }
         public string Password { get; set; }
         public string Photo { get; set; }
+
+        public string RoleId { get; set; }
         public Role Role { get; set; }
     }
 }
