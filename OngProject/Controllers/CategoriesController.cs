1bf674df2a6dfcba30180995e33f65896d7641fd
MNVillamarin <villamarin.matias@outlook.com>
2021-11-04 15:05:24 -0300

    MV: Ticket 100-47 Endpoint Listado de Categorías Se agrego el Auth al Get para solo Admin

--- OngProject/Controllers/CategoriesController.cs
+++ OngProject/Controllers/CategoriesController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using OngProject.Application.DTOs.Categories;
+using OngProject.Application.DTOs.Newss;
 using OngProject.Application.Services;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace OngProject.Controllers
 {
@@ -17,6 +20,14 @@ namespace OngProject.Controllers
         }
 
         [HttpGet]
+        [Authorize(Roles = "Admin")]
+        #region Documentation
+        [SwaggerOperation(Summary = "List of all Categories",Description = "Require admin privileges")]
+        [SwaggerResponse(200, "Success. Returns a list of existing Categories", typeof(GetCategoriesDto))]
+        [SwaggerResponse(400, "BadRequest. Something went wrong, try again")]
+        [SwaggerResponse(401, "Unauthenticated user or wrong jwt token")]
+        [SwaggerResponse(403, "Unauthorized user or wrong jwt token")]
+        #endregion
         public async Task<ActionResult> GetAll()
         {
             return Ok(await _service.GetAll());
