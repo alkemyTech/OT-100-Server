fbdcd48e1f62819c94c2f176ee5e028877af596f
Matias Villamarin <villamarin.matias@outlook.com>
2021-11-10 21:55:19 -0300

    sprint3-OT100-75-Endpoint Actualización Testimonios Se agrego documentacion de swagger, el return de la entidad y se chequeo las validaciones correspondientes.

--- OngProject/Controllers/TestimonialsController.cs
+++ OngProject/Controllers/TestimonialsController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OngProject.Application.DTOs.Testimonials;
 using OngProject.Application.Services;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace OngProject.Controllers
 {
@@ -36,11 +38,18 @@ namespace OngProject.Controllers
         }
 
         [HttpPut("{id}")]
-        public async Task<ActionResult> Update(int id, CreateTestimonyDto testimonyDto)
+        [Authorize(Roles = "Admin")]
+        #region Documentation
+        [SwaggerOperation(Summary = "Modifies an existing Testimony",Description = "Requires admin privileges")]
+        [SwaggerResponse(200, "Updated. Returns the object Testimony updated", typeof(GetTestimonialsDto))]
+        [SwaggerResponse(400, "BadRequest. Object not updated, try again")]
+        [SwaggerResponse(401, "Unauthenticated or wrong jwt token")]
+        [SwaggerResponse(403, "Unauthorized user")]
+        #endregion
+        public async Task<ActionResult<GetTestimonialsDto>> Update(int id, CreateTestimonyDto testimonyDto)
         {
             await _service.UpdateTestimony(id, testimonyDto);
-
-            return NoContent();
+            return await GetById(id);
         }
 
         [HttpDelete("{id}")]
