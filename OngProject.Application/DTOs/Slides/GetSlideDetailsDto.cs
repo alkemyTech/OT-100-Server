b02c001459d62149a82aa4e3392606047971838a
Samuel Terrazas <samuel-terrazas@outlook.com>
2021-11-12 10:44:18 -0300

    ST: sprint3-OT100-00-64 Endpoint Detalle Slides - sprint3-OT100-00-66 Endpoint Actualización Slides - sprint3-OT100-00-74 Endpoint Creación Testimonios - sprint3-OT100-00-69 Endpoint Creación Miembros

---
+++ OngProject.Application/DTOs/Slides/GetSlideDetailsDto.cs
@@ -0,0 +1,14 @@
+﻿using OngProject.Application.Mappings;
+using OngProject.Domain.Entities;
+
+namespace OngProject.Application.DTOs.Slides
+{
+    public class GetSlideDetailsDto : IMapFrom<Slide>
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string Text { get; set; }
+        public int Order { get; set; }
+        public int OrganizationId { get; set; }
+    }
+}
---
+++ OngProject.Application/DTOs/Slides/UpdateSlideDto.cs
@@ -0,0 +1,12 @@
+﻿using OngProject.Application.Mappings;
+using OngProject.Domain.Entities;
+
+namespace OngProject.Application.DTOs.Slides
+{
+    public class UpdateSlideDto : IMapFrom<Slide>
+    {
+        public string ImageUrl { get; set; }
+        public string Text { get; set; }
+        public int Order { get; set; }
+    }
+}
