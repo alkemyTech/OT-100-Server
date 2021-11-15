4591764994a677b9eeec910a1c370a82eb9824da
Samuel Terrazas <samuel-terrazas@outlook.com>
2021-11-15 11:16:58 -0300

    ST: sprint3-OT100-46 Servicio reutilizable para subida de imagenes.

---
+++ OngProject.Application/Helpers/Validations/FileSizeValidation.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace OngProject.Application.Helpers.Validations
+{
+    public class FileSizeValidation : ValidationAttribute
+    {
+        private readonly int _maxSizeInMegabytes;
+
+        public FileSizeValidation(int maxSizeInMegabytes)
+        {
+            _maxSizeInMegabytes = maxSizeInMegabytes;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is null)
+                return ValidationResult.Success;
+
+            IFormFile formFile = value as IFormFile;
+
+            if (formFile is null)
+                return ValidationResult.Success;
+
+            if (formFile.Length > _maxSizeInMegabytes * 1024 * 1024)
+                return new ValidationResult($"Maximum allowed file size is {_maxSizeInMegabytes} MB.");
+
+            return ValidationResult.Success;
+        }
+    }
+}
