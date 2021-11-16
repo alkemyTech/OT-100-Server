36d75faeb80b9e1cfff023f313e2c1bb5a54635d
Samuel Terrazas <samuel-terrazas@outlook.com>
2021-11-16 18:48:50 -0300

    ST: sprint4-OT100-94 Agregar documentación con Swagger

--- OngProject/Controllers/SlidesController.cs
+++ OngProject/Controllers/SlidesController.cs
@@ -10,6 +10,7 @@ namespace OngProject.Controllers
 {
     [ApiController]
     [Route("api/slides")]
+    [SwaggerTag("Create, Read, Update and Delete Slides")]
     public class SlidesController : ControllerBase
     {
         private readonly SlideService _service;
@@ -24,11 +25,11 @@ namespace OngProject.Controllers
         [Authorize(Roles = "Admin")]
         #region Documentation
         [SwaggerOperation(Summary = "Get slide details by id", Description = "Requires admin privileges")]
-        [SwaggerResponse(200, "Success. Returns the slide details.", typeof(GetSlideDetailsDto))]
-        [SwaggerResponse(400, "BadRequest. Something went wrong, try again.")]
+        [SwaggerResponse(200, "Success. Returns the slide details.")]
         [SwaggerResponse(404, "NotFound. Entity id not found.")]
         [SwaggerResponse(401, "Unauthenticated user or wrong jwt token.")]
         [SwaggerResponse(403, "Unauthorized user.")]
+        [SwaggerResponse(500, "Internal server error. An error occurred while processing your request.")]
         #endregion
         public async Task<ActionResult<GetSlideDetailsDto>> GetById(int id)
         {
@@ -41,9 +42,10 @@ namespace OngProject.Controllers
         [SwaggerOperation(Summary = "Modifies an existing Slide", Description = "Requires admin privileges")]
         [SwaggerResponse(200, "Updated. Returns nothing")]
         [SwaggerResponse(400, "BadRequest. Something went wrong, try again.")]
-        [SwaggerResponse(404, "NotFound. Entity id not found.")]
         [SwaggerResponse(401, "Unauthenticated or wrong jwt token.")]
         [SwaggerResponse(403, "Unauthorized user.")]
+        [SwaggerResponse(404, "NotFound. Entity id not found.")]
+        [SwaggerResponse(500, "Internal server error. An error occurred while processing your request.")]
         #endregion
         public async Task<ActionResult> Update(int id, UpdateSlideDto slideDto)
         {
@@ -56,10 +58,10 @@ namespace OngProject.Controllers
         [Authorize(Roles = "Admin")]
         #region Documentation
         [SwaggerOperation(Summary = "List of all Slides", Description = "Requires admin privileges")]
-        [SwaggerResponse(200, "Success. Returns a list of existing Slides", typeof(GetSlidesDto))]
-        [SwaggerResponse(400, "BadRequest. Something went wrong, try again")]
+        [SwaggerResponse(200, "Success. Returns a list of existing Slides")]
         [SwaggerResponse(401, "Unauthenticated user or wrong jwt token")]
         [SwaggerResponse(403, "Unauthorized user")]
+        [SwaggerResponse(500, "Internal server error. An error occurred while processing your request.")]
         #endregion
         public async Task<ActionResult<List<GetSlidesDto>>> GetAll()
         {
@@ -69,11 +71,12 @@ namespace OngProject.Controllers
         [HttpDelete("{id}")]
         [Authorize(Roles = "Admin")]
         #region Documentation
-        [SwaggerOperation(Summary = "Delete an existing Slide", Description = "Requires admin privileges")]
-        [SwaggerResponse(200, "Success. Returns nothing")]
-        [SwaggerResponse(400, "BadRequest. Something went wrong, try again")]
-        [SwaggerResponse(401, "Unauthenticated user or wrong jwt token")]
-        [SwaggerResponse(403, "Unauthorized user")]
+        [SwaggerOperation(Summary = "Soft delete an existing Slide", Description = "Requires admin privileges")]
+        [SwaggerResponse(204, "Deleted. Returns nothing.")]
+        [SwaggerResponse(401, "Unauthenticated user or wrong jwt token.")]
+        [SwaggerResponse(403, "Unauthorized user.")]
+        [SwaggerResponse(404, "NotFound. Entity id not found.")]
+        [SwaggerResponse(500, "Internal server error. An error occurred while processing your request.")]
         #endregion
         public async Task<ActionResult> Delete(int id)
         {
