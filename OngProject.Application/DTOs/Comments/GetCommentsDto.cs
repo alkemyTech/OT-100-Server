2eea306530f415b9b0906ac0c72f19af08cb3c4f
AnabellaMeluzzi <91091530+AnabellaMeluzzi@users.noreply.github.com>
2021-11-17 20:24:06 -0300

    AM: OT100-95 (#33)
    * AM: OT100-95
    
    * AM:OT100-90
    
    * AM: OT100-62
    
    * AM: Corrección endpoint Contacts.

---
+++ OngProject.Application/DTOs/Comments/GetCommentsDto.cs
@@ -0,0 +1,13 @@
+﻿using OngProject.Application.Mappings;
+using OngProject.Domain.Entities;
+using System;
+
+namespace OngProject.Application.DTOs.Comments
+{
+    public class GetCommentsDto : IMapFrom<Comment>
+    {
+        public string Body { get; set; }
+        public DateTime? CreatedAt { get; set; }
+
+    }
+}
