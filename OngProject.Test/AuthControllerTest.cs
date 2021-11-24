5eaf7279b5c29f7a7b9a5d5b79c678c0e4cd56b9
Matias Villamarin <villamarin.matias@outlook.com>
2021-11-23 23:56:42 -0300

    OT100-106 Test: Endpoints /authentication

---
+++ OngProject.Test/AuthControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace OngProject.Test
+{
+    public class AuthControllerTest : TestScenarioBase
+    {
+        [Fact]
+        public async Task ValidLoginAdminTest()
+        {
+            //Arrange
+            using (var server = CreateServer())
+            {
+                var request = new
+                {
+                    Url = "api/auth/login",
+                    Body = new
+                    {
+                        email = "admin@localhost",
+                        password = "Abc123."
+                    }
+                };
+
+                //Act
+                var response = await server.CreateClient().PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+                var value = await response.Content.ReadAsStringAsync();
+
+                //Assert
+                response.EnsureSuccessStatusCode();
+            }
+        }
+    }
+}
---
+++ OngProject.Test/TestScenarioBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+namespace OngProject.Test
+{
+    public class TestScenarioBase
+    {
+        public HttpClient Client { get; private set; }
+
+        public TestServer CreateServer()
+        {
+            var webHostBuilder = WebHost.CreateDefaultBuilder();
+            webHostBuilder.UseStartup<Startup>();
+            var testServer = new TestServer(webHostBuilder);
+            return testServer;
+        }
+    }
+}
