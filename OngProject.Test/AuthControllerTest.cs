d7f4f5c2fe8a478564fe08653034cb2e79e83151
Matias Villamarin <villamarin.matias@outlook.com>
2021-11-25 03:25:39 -0300

    OT100-106 Test: Endpoints /authentication Se agregaron los test a los 3 end points de AuthController

--- OngProject.Test/AuthControllerTest.cs
+++ OngProject.Test/AuthControllerTest.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace OngProject.Test
@@ -6,7 +9,7 @@ namespace OngProject.Test
     public class AuthControllerTest : TestScenarioBase
     {
         [Fact]
-        public async Task ValidLoginAdminTest()
+        public async Task ValidLoginTest()
         {
             //Arrange
             using (var server = CreateServer())
@@ -23,11 +26,133 @@ namespace OngProject.Test
 
                 //Act
                 var response = await server.CreateClient().PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
-                var value = await response.Content.ReadAsStringAsync();
 
                 //Assert
                 response.EnsureSuccessStatusCode();
             }
         }
+        
+        [Fact]
+        public async Task InValidLoginTest()
+        {
+            //Arrange
+            using (var server = CreateServer())
+            {
+                var request = new
+                {
+                    Url = "api/auth/login",
+                    Body = new
+                    {
+                        email = "adminlocalhost",
+                        password = "Abc123."
+                    }
+                };
+
+                //Act
+                var response = await server.CreateClient().PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+
+                //Assert
+                Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            }
+        }
+        
+        
+        
+        [Fact]
+        public async Task ValidRegisterTest()
+        {
+            //Arrange
+            using (var server = CreateServer())
+            {
+                var request = new
+                {
+                    Url = "api/auth/register",
+                    Body = new
+                    {
+                        email = $"{Guid.NewGuid().ToString()}@localhost",
+                        password = "Abc123."
+                    }
+                };
+
+                //Act
+                var response = await server.CreateClient().PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+
+                //Assert
+                response.EnsureSuccessStatusCode();
+            }
+        }
+        
+        [Fact]
+        public async Task InValidRegisterPasswordShortTest()
+        {
+            //Arrange
+            using (var server = CreateServer())
+            {
+                var request = new
+                {
+                    Url = "api/auth/register",
+                    Body = new
+                    {
+                        email = $"{Guid.NewGuid().ToString()}@localhost",
+                        password = "123"
+                    }
+                };
+
+                //Act
+                var response = await server.CreateClient().PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+
+                //Assert
+                Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            }
+        }
+        
+        
+        
+        [Fact]
+        public async Task ValidMeTest()
+        {
+            //Arrange
+            using (var server = CreateServer())
+            {
+                var request = new
+                {
+                    Url = "api/auth/me",
+                };
+                
+                var client = server.CreateClient();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",
+                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxZmUyYmVmMi00NmEyLTRmODktYjg1NC1hZDUxNzRjMmZiN2UiLCJlbWFpbCI6ImFkbWluQGxvY2FsaG9zdCIsInJvbGUiOiJBZG1pbiIsIm5iZiI6MTYzNzgyMDg5NiwiZXhwIjoxNjM3OTA3Mjk2LCJpYXQiOjE2Mzc4MjA4OTZ9.LUjqaOt4uhD9G-d_mvvts36gomT_eq7QJcBblONmFg4");
+
+                //Act
+                var response = await client.GetAsync(request.Url);
+                
+                //Assert
+                response.EnsureSuccessStatusCode();
+            }
+        }
+        
+        [Fact]
+        public async Task InValidMeTest()
+        {
+            //Arrange
+            using (var server = CreateServer())
+            {
+                var request = new
+                {
+                    Url = "api/auth/me",
+                };
+                
+                var client = server.CreateClient();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",
+                    "NoTokenRey");
+
+                //Act
+                var response = await client.GetAsync(request.Url);
+                
+                //Assert
+                Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+            }
+        }
+        
     }
 }
